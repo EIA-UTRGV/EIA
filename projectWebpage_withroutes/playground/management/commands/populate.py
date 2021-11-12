a8f9e639c6eac5c229fc2e13f2ab1b3da2996c09
roelbarrera <roel.barrera01@utrgv.edu>
2021-11-11 23:30:19 -0800

    Django Backend Database Work
    Added Django backend database work with Models.

---
+++ projectWebpage_withroutes/playground/management/commands/populate.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from playground.models import User, Stocks
+from datetime import date
+
+class Command(BaseCommand):
+
+	def handle(self, *args, **options):
+		create_user("roel","password")
+
+		create_stock("Apple Inc."
+		,"AAPL"
+		,"makes apple phones every year"
+		,147.87
+		,148.24,
+		"0.00 x 1100",
+		"0.00 x 1200",
+		40264924,
+		76143410,
+		"2.426T"
+		,date.today())
+
+def create_user(email,password):
+
+	user = User.objects.filter(email=email).first()
+	if user is None:
+		new_user = User()
+		new_user.email = email
+		new_user.password = password
+		new_user.save()
+	print(user)
+
+def create_stock(name,ticker,description,pclose,open,bid,ask,volume,avgvolume,mcap,sdate):
+
+	new_stock = Stocks.objects.filter(stock_ticker = ticker).first()
+	if ticker is None:
+		new_stock = Stocks()
+		new_stock.stock_name = name
+		new_stock.stock_ticker = ticker
+		new_stock.stock_description = description
+		new_stock.stock_pclose = pclose
+		new_stock.stock_open = open
+		new_stock.stock_bid = bid
+		new_stock.stock_ask = ask
+		new_stock.stock_volume = volume
+		new_stock.stock_avgvolume = avgvolume
+		new_stock.stock_mcap = mcap
+		new_stock.submission_date = sdate
+		new_stock.save()
---
+++ projectWebpage_withroutes/playground/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+TRILLION = 1000000000000
+class User(models.Model):
+	email = models.CharField(max_length=30)
+	password = models.CharField(max_length=30)
+
+class Stocks(models.Model):
+	stock_name =  models.CharField(max_length=30)
+	stock_ticker = models.CharField(max_length=30)
+	stock_description = models.CharField(max_length=100)
+	stock_pclose = models.FloatField()
+	stock_open = models.FloatField()
+	stock_bid = models.CharField(max_length=30)
+	stock_ask = models.CharField(max_length=30)
+	stock_volume = models.PositiveIntegerField(max_length=30)
+	stock_avgvolume = models.PositiveBigIntegerField(max_length=30)
+	stock_mcap = models.CharField(max_length=30)
+	submission_date = models.DateField()
