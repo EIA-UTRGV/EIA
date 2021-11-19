218c8d862193bd81f16e80a39f66dd80e406a637
roelbarrera <roel.barrera01@utrgv.edu>
2021-11-18 23:01:09 -0800

    Register Form working
    The register finally works, dumb error.

--- projectWebpage_withroutes/playground/forms.py
+++ projectWebpage_withroutes/playground/forms.py
@@ -2,18 +2,15 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
-from manager.models import Person
+from playground.models import User
 
 
 # Create your forms here.
 
-class NewUserForm(UserCreationForm):
-
-    class RegisterForm(ModelForm):
+class RegisterForm(ModelForm):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=PasswordInput)
     email = forms.CharField(widget=EmailInput)
-
     class Meta:
-        model = Person
-        fields = ["username", "password", "birthdate", "email"]
+        model = User
+        fields = ["email", "password"]
